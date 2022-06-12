9d297d7cc17f43f066792b1675ae0d554daf0a21
spnraju <padmanabharaju.sagi@gmail.com>
2022-06-12 14:19:25 +0530

    migrate to cypress 10

---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // configure tasks and plugins here
+    }
+  }
+})
