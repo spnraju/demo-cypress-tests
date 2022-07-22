66b4bb757458bcb97002c9b3e12de4dea7965bf1
spnraju <padmanabharaju.sagi@gmail.com>
2022-07-23 05:26:35 +0530

    add eslint and prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  extends: ['eslint:recommended', 'eslint-config-prettier'],
+  env: {
+    node: true,
+    es6: true,
+  },
+  globals: {
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {},
+};
